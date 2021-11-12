2920eb4962d30077ac4f2203e78fe121c621cc30
Tunazzinur Rahman Kabbo <19202103268@cse.bubt.edu.bd>
2021-11-12 20:47:16 +0600

    updated

--- Onine Shopping System - CSE-208.sql
+++ Onine Shopping System - CSE-208.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 28, 2021 at 07:39 PM
+-- Generation Time: Nov 12, 2021 at 03:22 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -35,6 +35,22 @@ CREATE TABLE `address` (
   `Customer_ID` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `address`
+--
+
+INSERT INTO `address` (`Address_ID`, `Apartment_Number`, `Street`, `Apartment_Name`, `Customer_ID`) VALUES
+('AID01', '089', 'Basundhara', 'Shanti Kunjo', 'CS01'),
+('AID02', '028', 'Basabo', 'Bilash Vobon', 'CS02'),
+('AID03', '087', 'Badda', 'Chondro Bilash', 'CS03'),
+('AID04', '042', 'Nikunjo', 'Krishno Chura', 'CS04'),
+('AID05', '052', 'Mogbazar', 'Jolpai Tower', 'CS05'),
+('AID06', '765', 'Pilkhana', 'Cresend Tower', 'CS06'),
+('AID07', '897', 'Banani', 'Chowdhury Villa', 'CS07'),
+('AID08', '987', 'Mirpur', 'Santi Niketon', 'CS08'),
+('AID09', '129', 'Savar', 'Varsity Colony', 'CS09'),
+('AID10', '943', 'Gulshan', 'Khan Villa', 'CS10');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +65,22 @@ CREATE TABLE `bill` (
   `Payment_ID` varchar(20) DEFAULT NULL,
   `Order_ID` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bill`
+--
+
+INSERT INTO `bill` (`Billing_ID`, `Billing_Date`, `Amount_Paid`, `Voucher_ID`, `Payment_ID`, `Order_ID`) VALUES
+('BID01', '20/10/2020', '2000', 'New10', 'PM01', 'OR01'),
+('BID02', '11/03/2020', '80000', 'New10', 'PM02', 'OR02'),
+('BID03', '03/11/2020', '140000', 'Free50', 'PM03', 'OR03'),
+('BID04', '21/05/2021', '90000', 'BTGO', 'PM04', 'OR04'),
+('BID05', '23/07/2021', '450', 'New10', 'PM05', 'OR05'),
+('BID06', '29/08/2021', '500', 'Free50', 'PM06', 'OR06'),
+('BID07', '20/09/2021', '1200', 'BTGO', 'PM07', 'OR07'),
+('BID08', '27/03/2021', '1300', 'CLR30', 'PM08', 'OR08'),
+('BID09', '25/09/2021', '3000', 'BTGO', 'PM09', 'OR09'),
+('BID10', '28/10/2021', '1300', 'Free50', 'PM10', 'OR10');
 
 -- --------------------------------------------------------
 
@@ -65,6 +97,22 @@ CREATE TABLE `customer` (
   `Customer_Type` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`Customer_ID`, `First_Name`, `Last_Name`, `Phone_Number`, `Email_Address`, `Customer_Type`) VALUES
+('CS01', 'Tunazzinur', 'Rahman', '01876787213', 'kabbo@gmail.com', 'Student'),
+('CS02', 'Zobayer', 'Hasan', '01615257555', 'zobayer@gmail.com', 'Student'),
+('CS03', 'Hamim', 'Reza', '01925671797', 'hamim@gmail.com', 'Teacher'),
+('CS04', 'Abu', 'Essa', '01643578850', 'essa@gmail.com', 'Employee'),
+('CS05', 'Rubiatul', 'Jannat', '01711553366', 'janu@gmail.com', 'Housewife'),
+('CS06', 'Al', 'Emam', '01365478945', 'emam@gmail.com', 'Farmer'),
+('CS07', 'Imran', 'Nazir', '01985412358', 'naziarmy@gmail.com', 'Plumber'),
+('CS08', 'Nayma', 'Khatun', '01874561236', 'nayema@gmail.com', 'Babysitter'),
+('CS09', 'Rina', 'Akhter', '01565478945', 'rina@gmail.com', 'Doctor'),
+('CS10', 'Shahida', 'Binte Uddin', '01633523596', 'sahida@gmail.com', 'Student');
+
 -- --------------------------------------------------------
 
 --
@@ -76,18 +124,21 @@ CREATE TABLE `customer category` (
   `Customer_ID` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `employee`
+-- Dumping data for table `customer category`
 --
 
-CREATE TABLE `employee` (
-  `Designation` varchar(20) DEFAULT NULL,
-  `Department` varchar(20) DEFAULT NULL,
-  `Join_Date` varchar(20) DEFAULT NULL,
-  `ssn` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `customer category` (`Customer_Category`, `Customer_ID`) VALUES
+('Student', 'CS01'),
+('Student', 'CS02'),
+('Teacher', 'CS03'),
+('Employee', 'CS04'),
+('Housewife', 'CS05'),
+('Farmer', 'CS06'),
+('Plumber', 'CS07'),
+('Babysitter', 'CS08'),
+('Doctor', 'CS09'),
+('Student', 'CS10');
 
 -- --------------------------------------------------------
 
@@ -98,12 +149,28 @@ CREATE TABLE `employee` (
 CREATE TABLE `employees` (
   `Employee_ID` varchar(20) NOT NULL,
   `Employee_Name` varchar(20) DEFAULT NULL,
-  `SSN` varchar(20) DEFAULT NULL,
+  `ssn` varchar(20) DEFAULT NULL,
   `Designation` varchar(20) DEFAULT NULL,
   `Employee_Type` varchar(20) DEFAULT NULL,
   `Salary` varchar(20) DEFAULT NULL,
   `Payment_ID` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`Employee_ID`, `Employee_Name`, `ssn`, `Designation`, `Employee_Type`, `Salary`, `Payment_ID`) VALUES
+('EID01', 'Tony stark', '445', 'Manager', 'Regular', '40000', 'PM01'),
+('EID02', 'Samira', '342', 'Cashier', 'Regular', '25000', 'PM02'),
+('EID03', 'Labiba', '289', 'Receptionist', 'Regular', '25000', 'PM03'),
+('EID04', 'Sam', '109', 'Assistant Manager', 'Regular', '30000', 'PM04'),
+('EID05', 'Stives', '209', 'Sales Man', 'Regular', '15000', 'PM05'),
+('EID06', 'Jack', '107', 'Sales Man', 'Regular', '15000', 'PM06'),
+('EID07', 'Natasa', '103', 'Sales Man', 'Regular', '15000', 'PM07'),
+('EID08', 'Karisma', '203', 'Sales Man', 'Regular', '15000', 'PM08'),
+('EID09', 'Qiana', '376', 'Sales Man', 'Regular', '15000', 'PM09'),
+('EID10', 'Jeet', '465', 'Product Manager', 'Regular', '25500', 'PM10');
 
 -- --------------------------------------------------------
 
@@ -116,6 +183,22 @@ CREATE TABLE `order product` (
   `Product_ID` varchar(20) NOT NULL,
   `Order_ID` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `order product`
+--
+
+INSERT INTO `order product` (`Quantity`, `Product_ID`, `Order_ID`) VALUES
+('1', 'P01', 'OR01'),
+('2', 'P02', 'OR02'),
+('4', 'P02', 'OR04'),
+('1', 'P02', 'OR06'),
+('1', 'P03', 'OR03'),
+('1', 'P04', 'OR08'),
+('1', 'P07', 'OR05'),
+('3', 'P07', 'OR07'),
+('2', 'P07', 'OR09'),
+('3', 'P09', 'OR10');
 
 -- --------------------------------------------------------
 
@@ -130,6 +213,22 @@ CREATE TABLE `orders` (
   `Shippent_Duration` varchar(20) DEFAULT NULL,
   `Payment_ID` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`Order_ID`, `Order_Date`, `Status`, `Shippent_Duration`, `Payment_ID`) VALUES
+('OR01', '23-10-2020', 'Delivered', '15 days', 'PM01'),
+('OR02', '25-01-2021', 'Delivered', '03 days', 'PM02'),
+('OR03', '23-09-2020', 'Delivered', '15 days', 'PM03'),
+('OR04', '21-03-2021', 'Delivered', '06 days', 'PM04'),
+('OR05', '21-06-2021', 'Delvered', '07 days', 'PM05'),
+('OR06', '12-05-2021', 'Returned', '10 days', 'PM06'),
+('OR07', '13-07-2021', 'Delivered', '15 days', 'PM07'),
+('OR08', '16-08-2021', 'Delivered', '12 days', 'PM08'),
+('OR09', '17-09-2021', 'Delivered', '02 days', 'PM09'),
+('OR10', '30-12-2020', 'Delivered', '10 days', 'PM10');
 
 -- --------------------------------------------------------
 
@@ -148,20 +247,21 @@ CREATE TABLE `payment` (
   `Visit_Number` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `person`
+-- Dumping data for table `payment`
 --
 
-CREATE TABLE `person` (
-  `First_Name` varchar(20) DEFAULT NULL,
-  `Last_Name` varchar(20) DEFAULT NULL,
-  `Email_Address` varchar(20) DEFAULT NULL,
-  `Phone_Number` varchar(20) DEFAULT NULL,
-  `ssn` varchar(20) NOT NULL,
-  `Date_Of_Birth` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `payment` (`Payment_ID`, `Payment_Mode`, `Card_Type`, `Card_Number`, `CVV`, `Name_On_Card`, `Customer_ID`, `Visit_Number`) VALUES
+('PM01', 'Card', 'Visa', '5527156302470003', '775', 'TUNAZZINUR RAHMAN', 'CS01', '#FGWS67'),
+('PM02', 'Card', 'Visa', '5527156302470004', '499', 'ZOBAYER HASAN', 'CS02', '#KLJDS89'),
+('PM03', 'Card', 'Visa', '5527156302470006', '556', 'HAMIM REZA', 'CS03', '#JTRF87'),
+('PM04', 'Card', 'MasterCard', '5527156302470042', '807', 'ABU ESSA', 'CS04', '#JHFRE89'),
+('PM05', 'Card', 'MasterCard', '5527156302470056', '310', 'RUBIATUL JANNAT', 'CS05', '#HJKF7'),
+('PM06', 'Card', 'MasterCard', '5527156302470078', '269', 'AL EMAM', 'CS06', '#JFTR8'),
+('PM07', 'Card', 'Visa', '5527156302470008', '275', 'IMRAN NAZIR', 'CS07', '#UHJFD897'),
+('PM08', 'Card', 'MasterCard', '5527156302470085', '456', 'NAYEMA KHATUN', 'CS08', '#YTGSD6'),
+('PM09', 'Card', 'Visa', '55271563024700012', '884', 'RINA AKHTER', 'CS09', '#425'),
+('PM10', 'Card', 'MasterCard', '5527156302470098', '776', 'SAHIDA BINTE UDDIN', 'CS10', '#FTSD7');
 
 -- --------------------------------------------------------
 
@@ -178,6 +278,22 @@ CREATE TABLE `product` (
   `Review_ID` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`Product_ID`, `Product_Name`, `Available_Number`, `Group_ID`, `Supplier_ID`, `Review_ID`) VALUES
+('P01', 'Rice-Cooker', '10', '100', 'SP01', 'PR01'),
+('P02', 'AC', '8', '100', 'SP01', 'PR01'),
+('P03', 'Laptop', '100', '100', 'SP01L', 'PR01'),
+('P04', 'Fridge', '15', '100', 'SP01', 'PR01'),
+('P05', 'Memory Card', '100', '100', 'SP01', 'PR01'),
+('P06', 'T-shirt', '100', '200', 'SP02', 'PR02'),
+('P07', 'Shirt', '100', '200', 'SP02', 'PR02'),
+('P08', 'Denim', '50', '200', 'SP02', 'PR02'),
+('P09', 'Boot', '40', '300', 'SP03', 'PR03'),
+('P10', 'Sneaker', '30', '300', 'SP03', 'PR03');
+
 -- --------------------------------------------------------
 
 --
@@ -192,6 +308,22 @@ CREATE TABLE `product details` (
   `Colour` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `product details`
+--
+
+INSERT INTO `product details` (`Product_ID`, `Weight`, `Width`, `Height`, `Colour`) VALUES
+('P01', '1.5kg', '15 inch', '10 inch', 'White'),
+('P02', '1.5 kg', '15 inch', '20 inch', 'White'),
+('P03', '1.9 kg', '15 inch', '10 mm', 'Black'),
+('P04', '160 lbs', '23 inch', '65 inch', 'Red'),
+('P05', '2 gm', '2 cm', '2 cm', 'Black'),
+('P06', '0.5 gm', '41 inch', '25.5 inch', 'Baby Pink'),
+('P07', '0.8 gm', '42 inch', '29 inch', 'Navy Blue'),
+('P08', '0.8 kg', '42 inch', '29 inch', 'Denim'),
+('P09', '1.5 kg', '25 cm', '14.5 cm', 'Brown'),
+('P10', '1 kg', '25 cm', '10 cm', 'White');
+
 -- --------------------------------------------------------
 
 --
@@ -202,6 +334,15 @@ CREATE TABLE `product group` (
   `Group_ID` varchar(20) NOT NULL,
   `Group_Name` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product group`
+--
+
+INSERT INTO `product group` (`Group_ID`, `Group_Name`) VALUES
+('100', 'Electronics'),
+('200', 'Clothing'),
+('300', 'Shoes');
 
 -- --------------------------------------------------------
 
@@ -216,6 +357,15 @@ CREATE TABLE `reviews` (
   `Review_Date` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`Quality_Rating`, `Defect%`, `Review_ID`, `Review_Date`) VALUES
+('100', '0', 'PR01', '23-01-2021'),
+('90', '10', 'PR02', '25-02-2021'),
+('85', '15', 'PR03', '20-03-2021');
+
 -- --------------------------------------------------------
 
 --
@@ -228,6 +378,16 @@ CREATE TABLE `supplier` (
   `Supply_Quantity` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`Supplier_ID`, `Supplier_Name`, `Supply_Quantity`) VALUES
+('SP01', 'Abu Bakkar', '33'),
+('SP01L', 'Abu Essa', '200'),
+('SP02', 'Cartel', '250'),
+('SP03', 'Nike', '70');
+
 -- --------------------------------------------------------
 
 --
@@ -238,6 +398,16 @@ CREATE TABLE `voucher` (
   `Voucher_ID` varchar(20) NOT NULL,
   `Discount%` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `voucher`
+--
+
+INSERT INTO `voucher` (`Voucher_ID`, `Discount%`) VALUES
+('BTGO', '25'),
+('CLR30', '30'),
+('Free50', '50'),
+('New10', '10');
 
 -- --------------------------------------------------------
 
@@ -251,6 +421,22 @@ CREATE TABLE `zip code` (
   `City` varchar(20) DEFAULT NULL,
   `Address_ID` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `zip code`
+--
+
+INSERT INTO `zip code` (`State`, `Zipcode_ID`, `City`, `Address_ID`) VALUES
+('Dhaka', '1210', 'Basabo', 'AID02'),
+('Dhaka', '1212', 'Badda', 'AID03'),
+('Dhaka', '1213', 'Banani', 'AID07'),
+('Dhaka', '1214', 'Gulshan', 'AID10'),
+('Dhaka', '1216', 'Mirpur', 'AID08'),
+('Dhaka', '1217', 'Mogbazar', 'AID05'),
+('Dhaka', '1220', 'Basundhara', 'AID01'),
+('Dhaka', '1229', 'Nikunjo', 'AID04'),
+('Dhaka', '1260', 'Pilkhana', 'AID06'),
+('Dhaka', '1344', 'Savar', 'AID09');
 
 --
 -- Indexes for dumped tables
@@ -285,13 +471,6 @@ ALTER TABLE `customer category`
   ADD PRIMARY KEY (`Customer_ID`);
 
 --
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`ssn`),
-  ADD KEY `Index30` (`ssn`);
-
---
 -- Indexes for table `employees`
 --
 ALTER TABLE `employees`
@@ -319,12 +498,6 @@ ALTER TABLE `orders`
 ALTER TABLE `payment`
   ADD PRIMARY KEY (`Payment_ID`),
   ADD KEY `Index11` (`Customer_ID`);
-
---
--- Indexes for table `person`
---
-ALTER TABLE `person`
-  ADD PRIMARY KEY (`ssn`);
 
 --
 -- Indexes for table `product`
